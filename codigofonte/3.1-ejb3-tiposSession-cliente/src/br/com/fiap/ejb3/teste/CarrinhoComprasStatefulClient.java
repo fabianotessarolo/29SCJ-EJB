b0c76c8cdf2f65ba10d3adc7611ad8b6a892f591
logonrm <logonrm@fiap.com.br>
2017-08-21 22:16:38 -0300

    Aula 4

---
+++ codigofonte/3.1-ejb3-tiposSession-cliente/src/br/com/fiap/ejb3/teste/CarrinhoComprasStatefulClient.java
@@ -0,0 +1,25 @@
+package br.com.fiap.ejb3.teste;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+import br.com.fiap.ejb3.session.CarrinhoCompras;
+
+public class CarrinhoComprasStatefulClient {
+
+	public static void main(String[] args) throws Exception {
+
+		Context context = new InitialContext();
+		CarrinhoCompras carrinhoCompras = null;
+		int contador = 10;
+
+		for (int j = 0; j < 10000000; j++) {
+			carrinhoCompras = (CarrinhoCompras) context.lookup("CarrinhoComprasBean/remote");
+			//Thread.sleep(200); //2 segundos
+		
+			carrinhoCompras.adicionarProduto(j);
+		}
+		
+		System.out.println("Quantidade de produtos adicionados: " + carrinhoCompras.verCarrinho().size());
+	}
+}
